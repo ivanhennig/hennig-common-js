f9472a9f281cde9cc1554dc51cd44ef619626549
Ivan <ivan@hennig.net.br>
2021-04-03 15:17:21 -0300

    Updated Vue experiment

---
+++ index.js
@@ -0,0 +1,6 @@
+export { default as BaseModal } from './src/components/BaseModal';
+export { default as H } from "./src/lib/H";
+export { initGrid } from "./src/lib/grid";
+export * from "./src/lib/notifications";
+export * from "./src/lib/templates";
+
--- src/index.js
+++
@@ -1,6 +0,0 @@
-export { default as BaseModal } from './components/BaseModal';
-export { default as H } from "./lib/H";
-export { initGrid } from "./lib/grid";
-export * from "./lib/notifications";
-export * from "./lib/templates";
-
