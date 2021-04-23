72cd5fd15f928bf1fcb6afccc94ed5cb675f8104
Ivan <ivan@hennig.net.br>
2021-04-23 13:21:23 -0300

    Fixed import

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export { default as BaseComponent } from './src/components/BaseComponent';
 export { default as BaseModal } from './src/components/BaseModal';
 export { default as H } from "./src/lib/H";
 export { initGrid } from "./src/lib/grid";
