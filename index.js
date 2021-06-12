a6b06807376c2e2be7916a022f4e98ba29f8a59d
Ivan Hennig <ivan@hennig.net.br>
2021-06-12 10:07:48 -0300

    Added new common component

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-export { default as BaseComponent } from './src/components/BaseComponent';
-export { default as BaseModal } from './src/components/BaseModal';
-export { default as H } from "./src/lib/H";
-export { initGrid } from "./src/lib/grid";
-export * from "./src/lib/notifications";
-export * from "./src/lib/templates";
+export { default as BaseComponent } from './src/components/BaseComponent'
+export { default as BaseModal } from './src/components/BaseModal'
+export { default as MultiSelectEditor } from './src/components/MultiSelectEditor'
+export { default as H } from './src/lib/H'
+export { initGrid } from './src/lib/grid'
+export * from './src/lib/notifications'
+export * from './src/lib/templates'
 
