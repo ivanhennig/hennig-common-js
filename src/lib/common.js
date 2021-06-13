b744b4afb4cbc563d64dcf79200a9f224314dce9
Ivan Hennig <ivan@hennig.net.br>
2021-06-13 16:39:00 -0300

    Removed, it's unneeded in Vue3

--- src/lib/common.js
+++ src/lib/common.js
@@ -22,33 +22,3 @@ export function valueAsArray (v) {
 
   return v
 }
-
-export function setArr (vueProp, key, values) {
-  if (!Array.isArray(values)) {
-    values = Object.values(values)
-  }
-
-  Vue.set(vueProp, key, values)
-}
-
-export function setObj (o, values, extra = {}, whitelist = null) {
-  const l_values = { ...extra, ...values }
-  for (const i in l_values) {
-    if (whitelist === null || whitelist.includes(i)) {
-      // eslint-disable-next-line no-prototype-builtins
-      if (l_values.hasOwnProperty(i)) {
-        Vue.set(o, i, l_values[i])
-      }
-    }
-  }
-}
-
-export function clearObj (o) {
-  for (const i in o) {
-    // eslint-disable-next-line no-prototype-builtins
-    if (o.hasOwnProperty(i)) {
-      Vue.set(o, i, null)
-      delete o[i]
-    }
-  }
-}
