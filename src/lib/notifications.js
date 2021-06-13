bebc0085c287cd7d855a7e82fb7e0ea9cb236103
Ivan Hennig <ivan@hennig.net.br>
2021-06-13 15:53:38 -0300

    Changed to Vue3

--- src/lib/notifications.js
+++ src/lib/notifications.js
@@ -1,43 +1,44 @@
-import Vue from 'vue'
-import Notifications from 'vue-notification'
-Vue.use(Notifications)
+import { notify } from '@kyvg/vue3-notification'
 
 /**
  * @param msg
  * @param callback
  */
-export function showSuccess (msg, callback = () => {}) {
-    Vue.notify({
-        group: 'main',
-        duration: 2000,
-        text: `
+export function showSuccess (msg, callback = () => {
+}) {
+  notify({
+    group: 'main',
+    duration: 2000,
+    text: `
 <div class="col-md-6 p-4 text-center" role="alert">
     <div class="check-mark"></div>
     <div class="font-weight-bold">${msg}</div>
     </div>
 </div>`,
-        type: 'success'
-    })
+    type: 'success'
+  })
 
-    setTimeout(callback, 2000)
+  setTimeout(callback, 2000)
 }
 
-export function showInfo (text, callback = () => {}) {
-    Vue.notify({
-        group: 'main',
-        duration: 500,
-        text,
-        type: 'success'
-    })
-    setTimeout(callback, 1000)
+export function showInfo (text, callback = () => {
+}) {
+  notify({
+    group: 'main',
+    duration: 500,
+    text,
+    type: 'success'
+  })
+  setTimeout(callback, 1000)
 }
 
-export function showError (text, callback = () => {}) {
-    Vue.notify({
-        group: 'main',
-        duration: 2000,
-        text,
-        type: 'error'
-    })
-    setTimeout(callback, 4000)
+export function showError (text, callback = () => {
+}) {
+  notify({
+    group: 'main',
+    duration: 2000,
+    text,
+    type: 'error'
+  })
+  setTimeout(callback, 4000)
 }
